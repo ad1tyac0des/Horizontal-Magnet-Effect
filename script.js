325457d040911da900c682f52a9dbaee9925eb22
Aditya Singh <adityasingh740745@gmail.com>
2024-09-14 16:44:42 +0530

    add magnet effect

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const frame = document.querySelector(".frame")
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+
+const dims = frame.getBoundingClientRect()
+const x1 = dims.x
+const x2 = dims.x + dims.width
 
 window.addEventListener("mousemove", function(dets){
     gsap.to("#crsr", {
@@ -10,13 +15,17 @@ window.addEventListener("mousemove", function(dets){
 })
 
 
-frame.addEventListener("mousemove", function(){
+frame.addEventListener("mousemove", function(dets){
     gsap.to("#crsr", {
-        scale: 6,
+        scale: 8,
     })
 
+    const alpha = gsap.utils.mapRange(x1, x2, 0, 1, dets.clientX) // alpha is Interpolation Factor for lerp fn
+    const moveX = lerp(-50, 50, alpha) // ouputs -50 on extreme left side and 50 on extreme right of frame
+
     gsap.to(".frame span", {
-        y: "-5vw",
+        x: moveX,
+        y: "-7vw",
         color: "#fff",
         duration: .3
     })
@@ -28,6 +37,7 @@ frame.addEventListener("mouseleave", function(){
     })
 
     gsap.to(".frame span", {
+        x: 0, // moves back element to its original posn
         y: 0,
         color: "#000",
         duration: .3
